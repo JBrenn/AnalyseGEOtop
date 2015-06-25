12f1f66b72cbf4ccb33c1a569f771aec10f8b9ed
z7H4AabqdYVj <jobrenne@outlook.com>
2015-06-25 15:03:38 +0200

    change tbl_obs, added Rmd for verification, changes in verification function

--- GEOtop_readValidationData.R
+++ GEOtop_readValidationData.R
@@ -3,7 +3,7 @@
 wpath <- "/run/user/1000/gvfs/smb-share:server=sdcalp01.eurac.edu,share=data2/Simulations/Simulation_GEOtop_1_225_ZH/Vinschgau/SimTraining/BrJ/HiResAlp/1D/Montecini_pnt_1_225_B2_011/"
 obs   <- list(hour=B2_h, day=B2_d)
 
-GEOtop_readValidationData <- function(wpath, observations, )
+GEOtop_readValidationData <- function(wpath, observations)
 {
   # source lookup_tbl
   data(lookup_tbl_observation)
@@ -18,5 +18,44 @@ GEOtop_readValidationData <- function(wpath, observations, )
   
   df_names <- as.data.frame(names(base))
   varPointIn <- merge(df_names, lookup_tbl_observation, by.x="names(base)", by.y = "obs_var")
+  varPointIn_what_direct <- varPointIn$geotop_what[varPointIn$geotop_where=="PointOutputFile"]
+  varPointIn_name_direct <- varPointIn$`names(base)`[varPointIn$geotop_where=="PointOutputFile"]
+  
+  # get x- , y-coordinates of output points
+  if (file.exists(file.path(wpath,"listpoints.txt")))
+  {
+    listpoints <- read.csv(file.path(wpath,"listpoints.txt"))
+    xpoints <- listpoints$xcoord
+    ypoints <- listpoints$ycoord
+  } else {
+    xpoints <- get.geotop.inpts.keyword.value("CoordinatePointX",wpath=wpath,numeric=T)
+    ypoints <- get.geotop.inpts.keyword.value("CoordinatePointY",wpath=wpath,numeric=T)
+  }
 
+  level <- 1:length(xpoints)
+  # read point data with specified keyword  
+  point_data <- get.geotop.inpts.keyword.value(keyword="PointOutputFile", wpath=wpath,
+                                                 raster=FALSE,
+                                                 data.frame=TRUE,
+                                                 #  start_date=as.POSIXct(start), 
+                                                 #  end_date=as.POSIXct(end),
+                                                 level=level, 
+                                                 date_field="Date12.DDMMYYYYhhmm.",
+                                                 tz="Etc/GMT+1")
+  
+  var_out <- list()
+  for (i in 1:length(varPointIn_what_direct)) 
+  {
+    name <- as.character(varPointIn_name_direct)[i]
+    var <- as.character(varPointIn_what_direct)[i]
+    i_split <- strsplit(as.character(var),"%")
+    
+    if (length(i_split[[1]])==1) {
+      var_out[[name]] <- point_data[,var]
+    } else {
+      var_out[[ i_split[[1]][1] ]] <- point_data[ ,i_split[[1]][1] ]
+      var_out[[ i_split[[1]][2] ]] <- point_data[ ,i_split[[1]][2] ]
+      var_out[[name]] <- point_data[ ,i_split[[1]][1] ] + point_data[ ,i_split[[1]][2] ]
+    }
+  }
 }
