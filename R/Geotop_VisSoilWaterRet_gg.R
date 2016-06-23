d8fb726480ab56a6c2cb1bcedbe8664a37778edb
JBrenn <jobrenne@outlook.com>
2016-06-23 19:03:54 +0200

    ggplot with sideplots for SWC and SWP V1

--- R/Geotop_VisSoilWaterRet_gg.R
+++ R/Geotop_VisSoilWaterRet_gg.R
@@ -10,23 +10,25 @@
 # observed_40 <- observedLaimburg[,c(3,5)]
 # 
 # observed <- observed_20
-# names(observed) <- c("SWC","SWP")
+# names(observed) <- c("SWC_20","SWP_20")
 
 Geotop_VisSoilWaterRet_gg <- function(alpha = 0.94, n = 1.5, theta_sat = 0.50, theta_res = 0.05, accurate = 10,
-                                      add_ref_curves = T, observed)
+                                      add_ref_curves = T, observed, colors = "red")
 {
   # soil water pressure head in centimeter
   psi <- seq(1,10000000,accurate)
   
   # volumetric soil water content in vol%
-  swc <- swc(psi = -psi, alpha = alpha, n = n, theta_sat = theta_sat, theta_res = theta_res) *100
-  
-  pFdata <- as.data.frame(cbind(swc, psi))
+  swc <- list()
+  for (i in 1:length(alpha))
+    swc[[i]] <- swc(psi = -psi, alpha = alpha[i], n = n[i], theta_sat = theta_sat[i], theta_res = theta_res[i]) *100
   
   datapoly1 <- data.frame(id = rep(1,4), x = c(1.8,1.8,2.5,2.5), y = c(0,60,60,0))
   datapoly2 <- data.frame(id = rep(1,4), x = c(2.5,2.5,4.2,4.2), y = c(0,60,60,0))
   
-  p <- ggplot(observed, aes(x = log10(SWP), y = SWC), log="x") + 
+  observed$depth <- as.factor(observed$depth)
+  
+  p <- ggplot(observed, aes(x = log10(SWP), y = SWC, group = depth), log="x") + 
     #stat_density2d(aes(alpha=..level..), geom="polygon") +
     #geom_density_2d(col="black", alpha=.5, lineend="round", linejoin="round", na.rm=TRUE) +
     #scale_alpha_continuous(limits=c(0,0.2), breaks=seq(0,0.2,by=0.025)) +
@@ -35,8 +37,8 @@ Geotop_VisSoilWaterRet_gg <- function(alpha = 0.94, n = 1.5, theta_sat = 0.50, t
     geom_polygon(datapoly2, mapping = aes(x = x, y = y), fill=rgb(0,1,0,.25), col="grey") +
     geom_text(mapping = aes(label="FieldCapacity", x=2.15, y=57), alpha=.6, col="darkgreen", size=4) +
     geom_text(mapping = aes(label="PermanentWiltingPoint", x=4.2, y=45), alpha=.6, col="darkgreen", size=4, angle=90) +
-    geom_point(colour="red", alpha=0.15, size=1) +
-    geom_line(data = pFdata, mapping = aes(x = log10(psi), y= swc), alpha = .5, col="red", size=2) +
+    geom_point(colour="darkgrey", alpha=0.1, size=1) +
+
     #coord_trans(x = "log10") +
     # scale_x_log10(
     #    breaks = scales::trans_breaks("log10", function(x) 10^x),
@@ -72,14 +74,77 @@ Geotop_VisSoilWaterRet_gg <- function(alpha = 0.94, n = 1.5, theta_sat = 0.50, t
       geom_text(data = txt, mapping = aes(label=lab, x=x, y=y), alpha=.6, size=3.5)
   }
   
-  if (all(is.na(observed$SWC))) {
-    gg <- ggExtra::ggMarginal(p, type = c("density"), margins = "x",  xparams = list(size=2))
-  } else if (all(is.na(observed$SWP))) {
-    gg <- ggExtra::ggMarginal(p, type = c("density"), margins = "y",  yparams = list(size=2))
-  } else {
-    gg <- ggExtra::ggMarginal(p, type = c("density"), margins = "both",  xparams = list(size=2),  yparams = list(size=2))
+  for (i in 1:length(swc))
+  {
+    pFdata <- as.data.frame(cbind(swc=swc[[i]], psi=psi))
+    p <- p + 
+      geom_line(data = pFdata, mapping = aes(x = log10(psi), y = swc), alpha = .5, col=colors[i], size=2)
   }
   
-  return(gg)
+  p + scale_color_manual(values = colors)
+  
+  # Marginal density plot of x (top panel)
+  if (!all(is.na(observed$SWP))) 
+  {
+    xdensity <- ggplot(observed, aes(x = log10(SWP), fill = depth)) + 
+      geom_density(alpha=.5) + 
+      scale_fill_manual(values = colors) + 
+      theme(legend.position = "none") +
+      theme(axis.title.x = element_blank(),
+            axis.text.x = element_blank())
+  }
+  
+  # Marginal density plot of y (right panel)
+  if (!all(is.na(observed$SWC))) 
+  {
+    ydensity <- ggplot(observed, aes(x = SWC, fill = depth)) + 
+      geom_density(alpha=.5) + 
+      scale_fill_manual(values = colors) + 
+      theme(legend.position = "none") + 
+      xlim(0,60) + 
+      coord_flip() + 
+      theme(axis.title.y = element_blank(),
+            axis.text.y = element_blank())
+  }
+  
+  if (exists("ydensity") & exists("xdensity")) {
+    
+    blankPlot <- ggplot() + geom_blank(aes(1,1)) +
+      theme(
+        plot.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(), 
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.line = element_blank()
+      )
+    
+    p <- grid.arrange(xdensity, blankPlot, p, ydensity, 
+                 ncol=2, nrow=2, widths=c(4, 1.4), heights=c(1.4, 4))
+  } else {
+    if (exists("xdensity"))
+    p <- grid.arrange(xdensity, p, 
+                      ncol=1, nrow=2, widths=c(10), heights=c(2.5,7.5))
+    
+    if (exists("ydensity"))
+    p <- grid.arrange(p, ydensity, 
+                       ncol=2, nrow=1, widths=c(7.5, 2.5), heights=c(10)) 
+  }
+  
+  
+  # if (all(is.na(observed$SWC))) {
+  #   gg <- ggExtra::ggMarginal(p, type = c("density"), margins = "x",  xparams = list(size=2))
+  # } else if (all(is.na(observed$SWP))) {
+  #   gg <- ggExtra::ggMarginal(p, type = c("boxplot"), margins = "y",  yparams = list(size=2))
+  # } else {
+  #   gg <- ggExtra::ggMarginal(p, type = c("density"), margins = "both",  xparams = list(size=2),  yparams = list(size=2))
+  # }
+  
+  return(p)
   
 }
